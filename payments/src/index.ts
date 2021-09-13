bfd9769e1bce3607a8835b2c9b4ede2d525a7cc5
arfan.uddin <arfan.uddin@voiceriver.com>
2021-09-12 21:56:11 -0700

    new startup message

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { OrderCancelledListener } from './events/listeners/oder-cancelled-listener';
 const start = async () => {
+	console.log('Starting ...');
 
 	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be define");
 
